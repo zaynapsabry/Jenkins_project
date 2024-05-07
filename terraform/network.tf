641e5367aebd1041892af360061b0b4539695dd6
zaynapsabry <zeinabsabry340@gmail.com>
2024-05-07 15:30:33 +0300

    adding terraform folder

---
+++ terraform/network.tf
@@ -0,0 +1,9 @@
+module "network" {
+  source = "network-module/"
+  region = var.region
+  vpc_cidr = var.vpc_cidr
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr_1 = var.private_subnet_cidr_1
+  private_subnet_cidr_2 = var.private_subnet_cidr_2
+  tag_name = var.tag_name
+}
